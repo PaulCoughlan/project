893b5d8b86689a097d021028f81c787c824a7db5
paulcoughlan <paulcoughlan7@gmail.com>
2015-12-02 16:59:36 +0000

    all_randomizers_done

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-var imagesArray = ["pics/Avengers.jpg", "pics/evilDead.jpg", "pics/matrix.jpg", "pics/bosses.jpg", "pics/dotd.jpg", "pics/kong.jpeg", "pics/mi.jpg", "pics/hatchet.jpg", "pics/polt.jpg", "pics/scream.jpg" ];
-var horrorArray = ["pics/evilDead.jpg", "pics/dotd.jpg", "pics/polt.jpg", "pics/scream.jpg"];
+var imagesArray = ["pics/Avengers.jpg", "pics/evilDead.jpg", "pics/matrix.jpg", "pics/bosses.jpg", "pics/dotd.jpg", "pics/kong.jpeg", "pics/mi.jpg", "pics/hatchet.jpg", "pics/polt.jpg", "pics/scream.jpg", "pics/crank.jpg", "pics/step.jpg", "pics/express.jpg", "pics/nacho.jpg", "pics/end.jpg",];
+var horrorArray = ["pics/evilDead.jpg", "pics/dotd.jpg", "pics/polt.jpg", "pics/scream.jpg", "pics/hatchet.jpg"];
+var actionArray = ["pics/Avengers.jpg", "pics/matrix.jpg", "pics/kong.jpeg", "pics/mi.jpg", "pics/crank.jpg",];
+var comedyArray = ["pics/bosses.jpg", "pics/step.jpg", "pics/express.jpg", "pics/nacho.jpg", "pics/end.jpg",];
 
 function imageDisplay(){
-    var rand = Math.floor(Math.random() * 10);
+    var rand = Math.floor(Math.random() * 15);
      document.canvas.src = imagesArray[rand];
 }
 
 function horror(){
-     var rand = Math.floor(Math.random() * 10);
+     var rand = Math.floor(Math.random() * 5);
      document.canvas.src = horrorArray[rand];
+}
+
+function action(){
+     var rand = Math.floor(Math.random() * 5);
+     document.canvas.src = actionArray[rand];
+}
+
+function comedy(){
+     var rand = Math.floor(Math.random() * 5);
+     document.canvas.src = comedyArray[rand];
 }

901ccec0aee2fdded9c130aa3b3a60beacd99637
paulcoughlan <paulcoughlan7@gmail.com>
2015-12-02 16:33:38 +0000

    added_horror_kinda_broken

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-var imagesArray = ["pics/Avengers.jpg", "pics/evilDead.jpg", "pics/matrix.jpg", ];
+var imagesArray = ["pics/Avengers.jpg", "pics/evilDead.jpg", "pics/matrix.jpg", "pics/bosses.jpg", "pics/dotd.jpg", "pics/kong.jpeg", "pics/mi.jpg", "pics/hatchet.jpg", "pics/polt.jpg", "pics/scream.jpg" ];
+var horrorArray = ["pics/evilDead.jpg", "pics/dotd.jpg", "pics/polt.jpg", "pics/scream.jpg"];
 
 function imageDisplay(){
-    var rand = Math.floor(Math.random() * 3);
+    var rand = Math.floor(Math.random() * 10);
      document.canvas.src = imagesArray[rand];
+}
+
+function horror(){
+     var rand = Math.floor(Math.random() * 10);
+     document.canvas.src = horrorArray[rand];
 }

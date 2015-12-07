5d7ad8f073b95355261e0c2f51a539d254adc51c
paulcoughlan <paulcoughlan7@gmail.com>
2015-12-07 05:12:29 +0000

    style

--- getrss.php
+++ getrss.php
@@ -1,8 +1,7 @@
 <?php
-//get the q parameter from URL
+
 $q=$_GET["q"];
 
-//find out which feed was selected
 if($q=="Rotten") {
   $xml=("http://www.rottentomatoes.com/syndication/rss/in_theaters.xml");
 } elseif($q=="Meta") {
@@ -12,7 +11,7 @@ if($q=="Rotten") {
 $xmlDoc = new DOMDocument();
 $xmlDoc->load($xml);
 
-//get elements from "<channel>"
+
 $channel=$xmlDoc->getElementsByTagName('channel')->item(0);
 $channel_title = $channel->getElementsByTagName('title')
 ->item(0)->childNodes->item(0)->nodeValue;
@@ -21,13 +20,13 @@ $channel_link = $channel->getElementsByTagName('link')
 $channel_desc = $channel->getElementsByTagName('description')
 ->item(0)->childNodes->item(0)->nodeValue;
 
-//output elements from "<channel>"
+
 echo("<p><a href='" . $channel_link
   . "'>" . $channel_title . "</a>");
 echo("<br>");
 echo($channel_desc . "</p>");
 
-//get and output "<item>" elements
+
 $x=$xmlDoc->getElementsByTagName('item');
 for ($i=0; $i<=2; $i++) {
   $item_title=$x->item($i)->getElementsByTagName('title')
